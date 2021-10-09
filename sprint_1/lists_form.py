262ea4b5fed2cef5b29130feccd21d32c9778bbb
Sergey Volkov <fenixguard1993@gmail.com>
2021-10-09 10:24:16 +0300

    list_form - the problem is solved

---
+++ sprint_1/lists_form.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple
+
+
+def get_sum(number_list: List[int], k: int) -> List[int]:
+    first_number = int(''.join(map(str, number_list)))
+    result = str(first_number + k)
+    return [int(x) for x in result]
+
+
+def read_input() -> Tuple[List[int], int]:
+    n = int(input())
+    number_list = list(map(int, input().strip().split()))
+    k = int(input())
+    return number_list, k
+
+
+number_list, k = read_input()
+print(" ".join(map(str, get_sum(number_list, k))))
