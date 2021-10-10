78e7e0b1549e49b3121e0dce7882849570d0cae4
Sergey Volkov <fenixguard1993@gmail.com>
2021-10-10 21:44:55 +0300

    Correction of the reviewer's comments, adding a description of the program logic

--- sprint_1/final_tasks/nearest_null.py
+++ sprint_1/final_tasks/nearest_null.py
@@ -1,48 +1,60 @@
 """
-ID успешного решения: 54192402
+ID успешного решения: 54320357
+
+Замечания по условию задачи.
+По условию задания нужно было обратить внимание на несколько моментов. Во-первых, длина входного массива - до 1 млн
+значений. Во-вторых, сами значения массива до 10^9. Исходя из этих данных ввод я решил сделать из файла, так как это
+быстрее, чем считывание из консоли. Также стоит отметить ограничение во времени 3 секунды для python. Отсюда можно
+понять, что алгоритм со сложностью больше линейной нам не подойдет. С учетом, что python сможет совершить в лучшем
+случае 100млн операций за секунду, то квадратичный алгоритм уйдет далеко за 3 секунды с длиной массива в 1 млн значений.
+
+Прицнип работы.
+Фукнция read_input считывает входные данные и передает их в функцию nearest_null.
+Идея алгоритма заключается в том, пройти массив слева направо проставить дистации, а потом справа налево и дозаполнить
+массив.
+Заводим результирующий массив result_dist длины n и заполняем его нулями.
+
+Алгоритм следующий. Идем по массиву вправо. До встречи первого нуля заполняем массив большим значением, например длиной
+массива. При встрече первого нуля в результирующий массив проставляем ноль и обновляем флаг нуля. Далее если нам
+встречаются значния отличные от нуля, то в результирующий массив добавляем значение предыдущей ячейки результата плюс 1.
+Таким образом проходим до конца. Начинаем движение обратно. До встречи первого нуля ничего не меняем в массиве. При
+встрече нуля обновляем флаг нуля, далее все значения отличные от нуля записываем в массив по правилу: берется
+минимальное значение из текущего значения и следующего плюс 1
+
+Вывод.
+Таким образом у нас получается сложность O(n), где n - длина входного массива. Вложенные циклы отсутствуют.
+Результаты записываются в выходной файл, для того, чтобы не тратить дополнительное время на принты в консоль.
+1.576s 113.63Mb
+
 """
-from typing import List, Tuple
+from typing import List, Tuple, NoReturn
 
 
 def nearest_null(n: int, sequence: List[int]) -> List[int]:
-    free_places_index = []
-    for i, s in enumerate(sequence):
-        if s == 0:
-            free_places_index.append(i)
-    left_free_place_index = free_places_index[0]
-    right_free_place_index = free_places_index[-1]
     result_dist = [0] * n
-    if len(free_places_index) == 1:
-        for i in range(left_free_place_index, len(sequence)):  # Проход вправо
-            if sequence[i] == 0:
-                result_dist[i] = 0
-            else:
+    length_sequence = len(sequence)
+
+    zero_flag = False
+    for i in range(length_sequence):  # Проход вправо ->
+        if sequence[i] == 0:
+            zero_flag = True
+            result_dist[i] = 0
+
+        else:  # sequence[i] =! 0
+            if zero_flag:
                 result_dist[i] = result_dist[i - 1] + 1
-        for i in reversed(range(left_free_place_index + 1)):  # Проход влево
-            if sequence[i] == 0:
-                result_dist[i] = 0
             else:
-                result_dist[i] = result_dist[i + 1] + 1
-        return result_dist
+                result_dist[i] = length_sequence
 
-    if len(free_places_index) == n:
-        return result_dist
+    zero_flag = False
+    for i in reversed(range(length_sequence)):  # Проход влево <-
+        if sequence[i] == 0:
+            zero_flag = True
+            result_dist[i] = 0
 
-    for i in range(left_free_place_index, len(sequence)):  # Проход вправо до индекса правого пустого места
-        if sequence[i] == 0:
-            result_dist[i] = 0
-        else:
-            result_dist[i] = result_dist[i - 1] + 1
-    for i in range(right_free_place_index, left_free_place_index, -1):  # Проход влево до индекса левого пустого места
-        if sequence[i] == 0:
-            result_dist[i] = 0
-        else:
-            result_dist[i] = min(result_dist[i], result_dist[i + 1] + 1)
-    for i in reversed(range(left_free_place_index + 1)):  # Проход влево от левого пустого места до начала массива
-        if sequence[i] == 0:
-            result_dist[i] = 0
-        else:
-            result_dist[i] = result_dist[i + 1] + 1
+        else:  # sequence[i] =! 0
+            if zero_flag:
+                result_dist[i] = min(result_dist[i], result_dist[i + 1] + 1)
 
     return result_dist
 
@@ -55,7 +67,11 @@ def read_input() -> Tuple[int, List[int]]:
     return n, sequence
 
 
-result = nearest_null(*read_input())
-with open('output.txt', 'w') as out_file:
-    out_file.write(" ".join(map(str, result)))
+def write_result(result: List[int]) -> NoReturn:
+    with open('output.txt', 'w') as out_file:
+        out_file.write(" ".join(map(str, result)))
 
+
+if __name__ == '__main__':
+    result = nearest_null(*read_input())
+    write_result(result)
--- sprint_1/final_tasks/sleight_of_hand.py
+++ sprint_1/final_tasks/sleight_of_hand.py
@@ -1,25 +1,34 @@
 """
-ID успешного решения: 54192576
+ID успешного решения: 54317536
+Замечания по условию задачи.
+Максимальная длина входного массива 16 значений. Количество раундов не превышает 9.
+Ограничение по времени 1 секунда. Жестких ограничений на сложность по условию нет.
+
+Прицнип работы.
+Функция read_input получает данные из консоли. Входные данные передаются в функцию sleight_of_hand.
+Так как мы часто будем использовать для сравнения количество нажатых кнопок ребятами, лучше вынести это в отдельную
+перменную press_buttons. А в переменную max_points будем складывать количество баллов.
+Функция get_count_buttons содержит вынесенную логику из фукнции read_input по заполнению словаря-счетчика кнопок в поле.
+Алгоритм следующий: берется очередной номер раунда из ключей словаря. Из словаря field_buttons берется значение
+по ключу текущего раунда. Если количество значений в словаре оказалось меньше либо равно количество одновременно
+нажатых кнопок ребятами, то они получают балл, в противном случае игра продолжается. Пройдя все раунды мы получаем
+максимально возможное количество очков, которые ребята смогли бы заработать.
+
+Вывод.
+Алгоритм работает за O(n) 1 <= n <= 4 в фукнции read_input
+Алгоритм работает за O(n^2) 1 <= n <= 4 в фукнции get_count_buttons (так как входное поле 4х4) внешний цикл по строкам,
+внутренний по символам строки, которые преобразуются в числа.
+Алгоритм работает за O(n) 1 <= n <= 9 в фукнции sleight_of_hand.
+
+63ms 3.98Mb
+
 """
-from typing import Dict, Set, Tuple
+from typing import Dict, Tuple, List
 
 
-def sleight_of_hand(k: int, field_buttons: Dict[int, int], rounds: Set[int]) -> int:
-    max_points = 0
-    press_buttons = k * 2
-    for r in rounds:
-        count_button = field_buttons.get(r)
-        if count_button <= press_buttons:
-            max_points += 1
-    return max_points
-
-
-def read_input() -> Tuple[int, Dict[int, int], Set[int]]:
-    k = int(input())
+def get_count_buttons(buttons: List[str]) -> Dict[int, int]:
     field_buttons = dict()
-    rounds = set()
-    for i in range(4):
-        row = input().strip()
+    for row in buttons:
         for r in row:
             if r.isdigit():
                 num = int(r)
@@ -27,10 +36,28 @@ def read_input() -> Tuple[int, Dict[int, int], Set[int]]:
                     field_buttons[num] += 1
                 else:
                     field_buttons[num] = 1
-                rounds.add(num)
-
-    return k, field_buttons, rounds
+    return field_buttons
 
 
-result = sleight_of_hand(*read_input())
-print(result)
+def sleight_of_hand(k: int, buttons: List[str]) -> int:
+    max_points = 0
+    press_buttons = k * 2
+    field_buttons = get_count_buttons(buttons)
+    for button in field_buttons.keys():
+        count_button = field_buttons.get(button)
+        if count_button <= press_buttons:
+            max_points += 1
+    return max_points
+
+
+def read_input() -> Tuple[int, List[str]]:
+    k = int(input())
+    buttons = list()
+    for _ in range(4):
+        buttons.append(input().strip())
+    return k, buttons
+
+
+if __name__ == '__main__':
+    result = sleight_of_hand(*read_input())
+    print(result)
